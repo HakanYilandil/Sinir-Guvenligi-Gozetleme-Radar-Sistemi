a4d2941f5807ab0dcf8630e7ccf763254d439ed5
HakanYilandil <47758342+HakanYilandil@users.noreply.github.com>
2019-05-21 16:38:06 +0300

    fonksiyon eklendi.

--- arduino.ino
+++ arduino.ino
@@ -38,3 +38,15 @@ void loop() {
   Serial.print(".");
   }
 }
+int calculateDistance(){ 
+   
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+  return distance;
+}
